899207c6e6c5a8f501d4cccf4de2af092c67a62d
Azad RIlvan <rilvanm1@southernct.edu>
2016-05-18 09:22:18 -0400

    add styling for found and unfound allergy informations

--- client/FoodItem/foodItem.js
+++ client/FoodItem/foodItem.js
@@ -1,3 +1,6 @@
+Meteor.subscribe('allergies');
+Meteor.subscribe('productIngredients');
+
 Template.foodItem.events({
     'click .keyboardimg': function(event){
         event.preventDefault();
@@ -31,6 +34,74 @@ Template.search.events({
   }
 });
 
+Template.foodDetails.helpers({
+  allergy : function(){
+    var allergy = Allergies.findOne({allergyName:"peanuts"}, {fields: {allergyName: 1} });
+    //console.log(allergy.allergyName);
+    return Allergies.find().fetch();
+  },
+
+  alertAllergy : function(name) {
+    const allergies = Allergies.find().fetch();
+    var productIngredients = Product.findOne({productName:name}, {fields: {ingredients: 1}});
+    //return Product.find({productName:"Pretzels"}).fetch();
+    let arr = new Array();
+    allergies.forEach(function (post) {
+      let ingredients= productIngredients.ingredients;
+      console.log(ingredients);
+      
+      //console.log("psot: "+post.allergyName);
+      if (ingredients.indexOf(post.allergyName) >= 0) {
+        arr.push(post.allergyName);
+        console.log("found");
+      }
+      console.log("array:"+arr);
+      
+      //console.log("Title of post " + ": "+ post.allergyName );
+      //return productIngredients;
+
+  });
+    return arr;
+  },
+  /*
+    while ( allergies.hasNext() ) {
+        aller = allergies.next();
+        console.log( aller.allergyName );
+    }
+  }*/
+  foundAllergy: function(name) {
+    const allergies = Allergies.find().fetch();
+    var productIngredients = Product.findOne({productName:name}, {fields: {ingredients: 1}});
+    //return Product.find({productName:"Pretzels"}).fetch();
+    let count=0;
+    let arr = new Array();
+    allergies.forEach(function (post) {
+      let ingredients= productIngredients.ingredients;
+      console.log(ingredients);
+      
+      //console.log("psot: "+post.allergyName);
+      if (ingredients.indexOf(post.allergyName) >= 0) {
+        arr.push(post.allergyName);
+        count++;
+        console.log("foundeddd");
+      }
+      
+        
+      
+      console.log("array:"+arr);
+      
+      //console.log("Title of post " + ": "+ post.allergyName );
+      //return productIngredients;
+
+    });
+    if(count>0) {
+      return true;
+    } else {
+    return false;
+    }
+  }
+  
+});
 
 
 
